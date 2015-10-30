5198fcd3644afeedff39810d2787a02711e63188
j-bb <jb.git@briaud.org>
2015-10-30 19:01:58 +0100

    No need to use su since we are root

--- setup/p0-foundation/run-as-root.sh
+++ setup/p0-foundation/run-as-root.sh
@@ -7,14 +7,9 @@
 ################
 
 begin_block "Run as root step"
-    su root
-
     apt-get install aptitude
     napt-get install sudo
     adduser $(server_user) sudo
 
     add_unique_line_to_file /etc/sudoers "$(server_user) ALL=(ALL:ALL) ALL" "#Added by $(product_name)" true
-
-    exit 0
-
 end_block "Run as root step"
